d23ef797aa12dc0b83f7d81180996e90bb21ac01
Adrian <adriansli@uia.no>
2022-09-12 22:16:33 +0200

    Mmmm

--- IS-115/Modul4/oppgave4-1.php
+++ IS-115/Modul4/oppgave4-1.php
@@ -1,4 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>I oppgaven under har jeg laget en enkel matrise med indeks-verdiene 0, 3, 5, 7, 8 og 15 og gitt de verdier. <br></p>
+    <p>Deretter har jeg printet tallenen både ved hjelp av print_r funksjonen, og ved hjelp av en foreach-løkke</p>
 <?php 
-$array = array(1, 2, 3, 4, 5, 8 => 1,  4 => 1, 19, 3 => 13); 
-print_r($array); 
-?> 
+    
+    /*Lag et lite script som oppretter en matrise med heltall som indeks. Matrisen skal ha indeksene 0, 3, 5, 
+    7, 8 og 15. Skriv deretter ut innholdet i matrisen ved hjelp av funksjonen print_r() og ved å bruke en 
+    løkke (som du lærte i forrige modul). */
+    
+    $matrise = array(0 => 10, 3 => 20, 5 => 30, 7 => 40, 8 => 50, 15 => 60); 
+    
+    print_r($matrise);
+    echo "<br><br>";
+
+    foreach ($matrise as $indeks => $verdi) { 
+    echo $indeks ."=". $verdi."<br>"; 
+    }
+?>
+</body>
+</html>
---
+++ IS-115/Modul4/oppgave4-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Lag et skjema som registrerer en ny bruker i systemet du utvikler. Skjemaet må inneholde all nødvendig 
+informasjon om brukeren (f.eks. navn, mobilnr., e-post osv.) og skal gi feilmelding dersom: 
+• obligatoriske felt mangler (hvilke felt må vises til brukeren) 
+ 
+Du kan ev. også prøve deg på feilmelding dersom:  
+• noen felt er feil utfylt (hvilke felt må vises til brukeren) – ikke krav! 
+ 
+Når  skjemaet  sendes,  skal  data  fra  skjemaet  lagres  i  en  matrise  og  skrives  ut  på  skjermen  på  en 
+oversiktlig måte  sammen med beskjed om  at  den nye  brukeren er  registrert.  Normalt ville  vi  lagret 
+denne informasjonen i en database, men det skal vi gjøre  litt senere. Lag en video hvor du beskriver og 
+demonstrerer koden. -->
+
+navn, mobilnr, epost
+
+    
+</body>
+</html>
