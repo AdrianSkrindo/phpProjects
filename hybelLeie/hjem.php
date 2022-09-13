901589b78b57055ad5bc14bb184090aba8f3c5c9
AdrianSkrindo <adriansli@uia.no>
2022-09-13 22:08:54 +0200

    Hybelprsjeket arbeid

--- hybelLeie/hjem.php
+++ hybelLeie/hjem.php
@@ -77,13 +77,19 @@
 
 echo build_table($hybelArray); */
 ?>
-<div class="wrapper">
-    <div class="box"><img src="crusher.jpg" alt="a balloon"></div>
-    <div class="box"><img src="crusher.jpg" alt="a balloon"></div>
-    <div class="box"><img src="crusher.jpg" alt="a balloon"></div>
-    <div class="box"><img src="crusher.jpg" alt="a balloon"></div>
-    <div class="box"><img src="crusher.jpg" alt="a balloon"></div>
-</div>   
+<section class="basic-grid">
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')"><div class="cardtxt">1</div></div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">2</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">3</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')">4</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')">5</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">6</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1575517111478-7f6afd0973db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">7</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1598228723793-52759bba239c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">8</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60')">9</div>
+    <div class="card" style="background-image:url('https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60')">10</div>
+
+</section>   
 
 
 
