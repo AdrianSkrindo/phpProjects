1ff7479a73d74b1c36c51dbbcb5095a9cc558bda
AdrianSkrindo <adriansli@uia.no>
2022-10-06 18:57:52 +0200

    hmhm

--- IS-115/Modul5/oppgave5-5.php
+++ IS-115/Modul5/oppgave5-5.php
@@ -18,4 +18,29 @@ echo "<br>";
 $search = 'asdf' ;
 $trimmed = str_replace($search, '', $formatted) ;
 echo $trimmed ;
+
+echo "<br><br>";
+echo "Din input er:";
+if (isset($_REQUEST['trykk'])) {
+    echo $_REQUEST['input'];
+}
 ?> 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        <form method="post" action="">
+            Input: <input type="text" name="input" placeholder="Input" required><br>
+            <input type="submit" name="trykk" value="Submit">
+        </form>
+    </pre>
+</body>
+</html>
