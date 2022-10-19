5732b19533ec4c6581c626ce436d5bb45a63d9e4
Adrian <adriansli@uia.no>
2022-10-20 00:40:45 +0200

    Portfolio fremgang

--- portofolio/index.php
+++ portofolio/index.php
@@ -6,10 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script defer src="app.js"></script>
 </head>
 <body>
 
-    <h1 class="glow">adrian skrindo lindland</h1>
+  <!--  <h1 class="glow">adrian skrindo lindland</h1>-->
+
+  <section class="hidden">
+    <h1>Hallais!</h1>
+    <p>This is my portfolio</p>
+  </section>
+
+  <section class="hidden">
+    <h2>Some random text</h2>
+    <p>This is my website</p>
+  </section>
+
+  <section class="hidden">
+    <h2>More random text</h2>
+    <p>This is my website</p>
+  </section>
+
+
 
 </body>
 </html>
