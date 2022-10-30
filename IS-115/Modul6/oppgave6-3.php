165769a4d94074b2580c988a629aaf00908793e9
AdrianSkrindo <adriansli@uia.no>
2022-10-30 12:10:24 +0100

    Fullført modul 6, utenom oppgave 5

--- IS-115/Modul6/oppgave6-3.php
+++ IS-115/Modul6/oppgave6-3.php
@@ -1,45 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
+    <?php
+
+    $matrise = array();
+
+    class Bruker
+    {
+        var $forNavn;
+        var $etterNavn;
+        protected $brukerNavn;
+        protected $reg;
 
 
 
-        class Bruker {
-            var $forNavn;
-            var $etterNavn;
-            var $reg;
-            var $brukerNavn;
-
-
-            public function __construct()
-            {
-                self::$brukerNavn = "hei";
-                self::$reg = date("Y/m/d");
-            }
-
-
-        
+        public function __construct($forNavn, $etterNavn)
+        {
+            $this->forNavn = $forNavn;
+            $this->etterNavn = $etterNavn;
+            $alle_bok = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrtuvwxyz";
+            $this->brukerNavn = substr(str_shuffle($alle_bok), 0, 6);
+            $this->reg = date("Y/m/d");
         }
 
-        $bruker1 = new Bruker;
-        $bruker1->forNavn = "Adrian";
-        $bruker1->etterNavn = "Lindland";
+        public function __destruct()
+        {
+            $matrise[] = $this->hentBrukernavn();
+            print_r($matrise);
+        }
 
-        echo $bruker1 -> forNavn;
-        echo "<br>";
-        echo $bruker1 -> etterNavn;
-        echo "<br>";
-        echo $bruker1 -> reg;
+        function hentFornavn()
+        {
+            return $this->fornavn;
+        }
 
-        
+        function hentBrukernavn()
+        {
+            return $this->brukerNavn;
+        }
+
+        function hentReg()
+        {
+            return $this->reg;
+        }
+    }
+
+    $bruker1 = new Bruker("Adrian", "Skrindo");
+
+    echo $bruker1->forNavn;
+    echo "<br>";
+    echo $bruker1->hentBrukernavn();
+    echo "<br>";
+    echo $bruker1->hentReg();
+    echo "<br>";
+    echo "<br>";
+
+
 
     ?>
 </body>
+
 </html>
