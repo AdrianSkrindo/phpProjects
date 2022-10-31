c6ffa17a48b403b64734153df03dbd3396b6b810
AdrianSkrindo <adriansli@uia.no>
2022-10-31 16:47:19 +0100

    Filstruktur

--- IS-115/Notater/index.php
+++ IS-115/Notater/index.php
@@ -11,16 +11,25 @@
     <h1>Divere notater</h1>
 <div class="center">
 <a href="booleanCheck.php"> Boolean</a> <br>
+<a href="bruker.php"> Bruker</a> <br>
+<a href="delete.php"> Delete</a> <br>
 <a href="html_external.php"> External</a> <br>
 <a href="html_internal.php"> Internal</a> <br>
-<a href="Oving1.php"> Øving</a> <br>
-<a href="switchOving.php"> Switch</a> <br>
-<a href="toTall.php"> To like tall</a> <br>
+<a href="insert.php"> Insert</a> <br>
 <a href="matrise.php"> Matrise øving</a> <br>
-<a href="registrer.php"> Registreringskjema</a> <br>
+<a href="Oving1.php"> Øving</a> <br>
 <a href="OvingMatrise1.php"> Øving matrise</a> <br>
 <a href="OvingMatrise2.php"> Øving matrise 2</a> <br>
 <a href="OvingMatrise3.php"> Øving matrise 3</a> <br>
+<a href="querystring.php"> Querystring</a> <br>
+<a href="registrer.php"> Registreringskjema</a> <br>
+<a href="select.php"> Select</a> <br>
+<a href="session.php"> Session</a> <br>
+<a href="switchOving.php"> Switch</a> <br>
+<a href="toTall.php"> To like tall</a> <br>
+<a href="update.php"> Update</a> <br>
+
+
 </div>
 
 </body>
