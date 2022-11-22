4a5e46180a07287566def4107a79b84b5ef35991
AdrianSkrindo <adriansli@uia.no>
2022-11-22 15:10:23 +0100

    Startet litt på oppgave 9-2

--- IS-115/Modul9/oppgave9-2.php
+++ IS-115/Modul9/oppgave9-2.php
@@ -8,5 +8,33 @@
 </head>
 <body>
     
+<?php
+    /* File meta data */
+    $dir = "files/";
+    $filename = "m9.txt";
+
+    /* No directory with that name? */
+    if(!file_exists($dir)) 
+    {
+        if (!mkdir($dir, 0777, true)) 
+            die("Cannot create directory..." . $dir);
+    }
+    
+    /* File handler */
+    $fh = fopen($dir . $filename, "w");
+
+    /* File data */
+    $txt  = "Data was written to file: " . date('d.m.Y k\l. H:i') . " \r\n";
+    $txt .= "Filename: " . $dir . $filename . " \r\n";
+    
+    if($r = fwrite($fh, $txt))
+        echo $r . " bytes were written to file " . $filename . ".";
+    else
+        echo "An error occured.";
+
+    fclose($fh);
+?>
+
+
 </body>
 </html>
